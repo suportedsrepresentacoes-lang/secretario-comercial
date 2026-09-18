e427d62cde52616999cbd80602e1b42c4da0b65c
suportedsrepresentacoes-lang <suportedsrepresentacoes-lang@users.noreply.github.com>
2026-09-18 06:15:21 +0000

    deploy: 3173f2e008dd9998361b47d27349915805ba07ea

---
+++ assets/x-C8rzCGx_.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-WDGw8RfF.js";var t={name:`plus`,size:24,node:[[`path`,{d:`M5 12h14`,key:`1ays0h`}],[`path`,{d:`M12 5v14`,key:`s699le`}]]};t.node;var n=e(t),r={name:`x`,size:24,node:[[`path`,{d:`M18 6 6 18`,key:`1bl5f8`}],[`path`,{d:`m6 6 12 12`,key:`d8bk6v`}]]};r.node;var i=e(r);export{n,i as t};
